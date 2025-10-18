576a5e32eec12529eb78a25856d069a2d48aef6c
Muhammad Ali <kustalics@gmail.com>
2025-10-18 12:58:44 +0500

    Working on Task Services

--- lib/services/task.dart
+++ lib/services/task.dart
@@ -82,4 +82,69 @@ class TaskServices {
       throw Exception(e.toString());
     }
   }
+
+  ///Search Task
+  Future<TaskListingModel> search({
+    required String token,
+    required String searchKey,
+  }) async {
+    try {
+      http.Response response = await http.get(
+        Uri.parse('$todoURL/todos/search?keywords=$searchKey'),
+        headers: {'Authorization': token},
+      );
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        return TaskListingModel.fromJson(jsonDecode(response.body));
+      } else {
+        throw Exception(response.reasonPhrase.toString());
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  ///Update Task
+  Future<bool> updateTask({
+    required String token,
+    required String description,
+    required String taskID,
+  }) async {
+    try {
+      http.Response response = await http.patch(
+        Uri.parse('$todoURL/todos/update/$taskID'),
+        headers: {'Authorization': token, 'Content-Type': 'application/json'},
+        body: jsonEncode({'description': description}),
+      );
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        return true;
+      } else {
+        throw Exception(response.reasonPhrase.toString());
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  ///Delete Task
+  Future<bool> deleteTask({
+    required String token,
+    required String taskID,
+  }) async {
+    try {
+      http.Response response = await http.delete(
+        Uri.parse('$todoURL/todos/delete/$taskID'),
+        headers: {'Authorization': token},
+      );
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        return true;
+      } else {
+        throw Exception(response.reasonPhrase.toString());
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
 }
